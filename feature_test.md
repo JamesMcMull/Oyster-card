57c2fc759d3251c92cdafa3e27f968fe221a7cf2
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-04 15:52:47 +0100

    completed challenge 5 #top_up added

--- feature_test.md
+++ feature_test.md
@@ -1,3 +1,7 @@
 card = Oystercard.new
 
 card.balance # should return 0
+
+card.top_up(100)
+
+card.balance # should return 100
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,11 +1,19 @@
 require 'oystercard'
+
 describe Oystercard do
   it " has a balance of 0" do
-  #arrange
-  card = Oystercard.new
-  #act
-  balance = card.balance
-  #assert
-  expect(balance).to eq(0) 
+    #arrange
+    card = Oystercard.new
+    #act
+    balance = card.balance
+    #assert
+    expect(balance).to eq(0) 
+  end
+
+  describe '#top_up' do
+    it 'adds a given amount to the balance' do
+      card = Oystercard.new
+      expect { card.top_up(100) }.to change { card.balance }.from(0).to(100)
+    end
   end
 end
