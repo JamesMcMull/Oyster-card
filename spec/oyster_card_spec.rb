52ca0dfb174887b12b10032bbb6ae95b7251f766
Elliott <ells101@hotmail.co.uk>
2021-05-04 15:23:27 +0100

    step 4 adding balance complete

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,2 +1,11 @@
+require 'oystercard'
 describe Oystercard do
+  it " has a balance of 0" do
+  #arrange
+  card = Oystercard.new
+  #act
+  balance = card.balance
+  #assert
+  expect(balance).to eq(0) 
+  end
 end
